79fadf7fefcea534e57df1d0cf8e07de0edc9fe2
yanjingfeng <yanjingfeng@happycfc.com>
2020-04-01 18:14:20 +0800

    add_websocket

---
+++ websocket/src/main/java/com/study/websocket/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.study.websocket.controller;
+
+import com.study.websocket.bean.Greeting;
+import com.study.websocket.message.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
